a85e5e0b1be16b6f1380c517940d28daf741653b
Luciano <41302084+Rossi-Luciano@users.noreply.github.com>
2025-08-30 00:24:17 +0000

    exemplo de código

--- tests/test_aula_03.py
+++ tests/test_aula_03.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from aula_03 import TaskManager
+from src import aula_03
 
 class TestTaskManeger(TestCase):
     def test_add_number(self):
-        tk = TaskManager()
+        tk = aula_03.TaskManager()
         
         self.assertEqual(tk.add_task(1), "Número como task")
