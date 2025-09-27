6f76e6647ecbb88861faa6287983b4c83c8e584b
Luciano <41302084+Rossi-Luciano@users.noreply.github.com>
2025-09-27 23:47:27 +0000

    remove arquivos antigos

--- README.md
+++
@@ -1,2 +0,0 @@
-# teste_de_software
-Repositório dedicado à criação de exemplos e modelos para a disciplina de Teste de Software.
--- tests/test_aula_03.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from src import aula_03
-
-class TestTaskManeger(TestCase):
-    def test_add_number(self):
-        tk = aula_03.TaskManager()
-        
-        self.assertEqual(tk.add_task(1), "Número como task")
